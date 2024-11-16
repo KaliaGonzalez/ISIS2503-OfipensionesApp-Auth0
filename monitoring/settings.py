f775f379404efb265895f508038ae1a16615359b
KaliaGonzalez <k.gonzalezj@uniandes.edu.co>
2024-11-16 11:03:10 -0500

    broooo funciona yaa ombe

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'balances',
-    'estudiantes', 
+    'Balance',
+    'Estudiante', 
     'social_django',
 ]
 
