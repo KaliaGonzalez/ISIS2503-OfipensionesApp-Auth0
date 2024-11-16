ed03d6b3d3a61a519d2dbf5e486b245d3d5fb6f1
KaliaGonzalez <k.gonzalezj@uniandes.edu.co>
2024-11-16 12:10:25 -0500

    cambiesss

--- Estudiante/logic/variable_logic.py
+++ Estudiante/logic/variable_logic.py
@@ -1,12 +1,19 @@
 from ..models import Estudiante
+from django.core.exceptions import ObjectDoesNotExist
 
 def get_estudiantes():
     queryset = Estudiante.objects.all()
     return (queryset)
 
-def get_estudiante(id):
+"""def get_estudiante(id):
     estudiante = Estudiante.objects.raw("SELECT * FROM Estudiante_estudiante WHERE id=%s" % id)[0]
-    return (estudiante)
+    return (estudiante)""" 
+def get_estudiante(id): 
+    try: 
+        estudiante = Estudiante.objects.get(id=id)
+        return estudiante
+    except ObjectDoesNotExist:
+        return None
 
 def create_estudiante(form):
     estudiante = form.save()
